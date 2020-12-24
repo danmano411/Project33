a04e9a757f16b532c1c81984949f957d69784ef0
Dan Mano <danmano411@gmail.com>
2020-12-23 19:51:43 -0500

    added a score funtion to the game

--- sketch.js
+++ sketch.js
@@ -3,11 +3,16 @@ var Engine = Matter.Engine,
   Events = Matter.Events,
   Bodies = Matter.Bodies;
  
-var particles = [];
+var particle;
 var plinkos = [];
+var divisions = [];
 
 var divisionHeight=300;
 var score =0;
+
+var count = 0;
+var gameState = "play";
+
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
@@ -54,26 +59,72 @@ function setup() {
 function draw() {
   background("black");
   textSize(20)
- //text("Score : "+score,20,30);
+  text("Score : "+score,20,30);
   Engine.update(engine);
  
   
    for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
-   if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
-     score++;
-   }
- 
-  for (var j = 0; j < particles.length; j++) {
-   
-     particles[j].display();
+    plinkos[i].display();
    }
    for (var k = 0; k < divisions.length; k++) {
      
      divisions[k].display();
    }
+
+   if (particle != null){
+     particle.display();
+     if (particle.body.position.y > 760){
+       if (particle.body.position.x < 300){
+        score += 500;
+        particle = null
+        if (count >= 5) gameState = "end";
+       }
+     }
+   }
+
+   if (particle != null){
+    particle.display();
+    if (particle.body.position.y > 760){
+      if (particle.body.position.x > 300 && particle.body.position.x < 600){
+       score += 100;
+       particle = null
+       if (count >= 5) gameState = "end";
+      }
+    }
+  }
+
+  if (particle != null){
+    particle.display();
+    if (particle.body.position.y > 760){
+      if (particle.body.position.x > 600){
+       score += 200;
+       particle = null
+       if (count >= 5) gameState = "end";
+      }
+    }
+  }
+
+  if (gameState === "end"){
+    textSize(69);
+    text("Game Over", 240, 250)
+  }
+
+  textSize(32)
+  text(500, 10, 530)
+  text(500, 90, 530)
+  text(500, 170, 530)
+  text(500, 250, 530)
+  text(100, 330, 530)
+  text(100, 410, 530)
+  text(100, 490, 530)
+  text(200, 570, 530)
+  text(200, 650, 530)
+  text(200, 730, 530)
+}
+
+function mousePressed(){
+  if (gameState != "end"){
+    particle = new Particle(mouseX, 10, 10)
+    count++
+  }
 }
